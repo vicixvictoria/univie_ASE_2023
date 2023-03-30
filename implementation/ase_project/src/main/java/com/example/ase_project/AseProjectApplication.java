40c567388192d2139bc8f7ed218e1027171e4c2d
Michal Zak <michal.r.zak@protonmail.com>
2023-03-30 19:15:41 +0200

    init repo

---
+++ implementation/ase_project/src/main/java/com/example/ase_project/AseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ase_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AseProjectApplication.class, args);
+    }
+
+}
---
+++ implementation/ase_project/src/test/java/com/example/ase_project/AseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ase_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
