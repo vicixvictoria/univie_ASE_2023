2498d3cb01058fc214a42cca9366c5a75a345c05
Fabian Schmon <a01568351@unet.univie.ac.at>
2023-04-17 18:26:18 +0200

    created AttendanceServiceTest

---
+++ implementation/ase_project/src/test/java/com/example/ase_project/attendance/AttendanceServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.ase_project.attendance;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.example.ase_project.attendance.AttendanceService.attend;
+import static com.example.ase_project.attendance.AttendanceService.attendance;
+
+public class AttendanceServiceTest {
+    @Test
+    public void attendanceTest() {
+        Assertions.assertEquals(attend((long)100) , 1);
+        Assertions.assertEquals(attendance((long)100) , 1);
+        Assertions.assertEquals(attendance((long)200) , 0);
+    }
+}
