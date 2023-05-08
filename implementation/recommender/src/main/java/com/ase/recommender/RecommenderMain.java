d4cbc20b0951361b23fe4e0d1b1c8059e47db8d1
Fabian Schmon <a01568351@unet.univie.ac.at>
2023-05-08 18:30:29 +0200

    enforce code style

--- implementation/recommender/src/main/java/com/ase/recommender/RecommenderMain.java
+++ implementation/recommender/src/main/java/com/ase/recommender/RecommenderMain.java
@@ -1,4 +1,5 @@
 package com.ase.recommender;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
