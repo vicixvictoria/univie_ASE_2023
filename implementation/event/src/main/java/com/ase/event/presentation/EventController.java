43b8b255f8d683c0225630fdb95294d3bd054f3e
davidk97 <davidk97@unet.univie.ac.at>
2023-06-07 10:48:34 +0200

    bug fix

--- implementation/event/src/main/java/com/ase/event/presentation/EventController.java
+++ implementation/event/src/main/java/com/ase/event/presentation/EventController.java
@@ -5,7 +5,6 @@ import com.ase.event.business.EventService;
 import java.lang.invoke.MethodHandles;
 import java.util.Collection;
 import java.util.Date;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
